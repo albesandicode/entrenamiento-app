9ba9311334c432a6ea5d877eda2b8bc956ab6948
Alberto Sánchez <albesandicode@gmail.com>
2026-09-21 16:26:01 +0200

    Añade el inicio de JS para la V1

---
+++ app.js
@@ -0,0 +1,20 @@
+let entrenamiento = [
+    {
+        fecha: "21/09",
+        ejercicios: [
+            { nombre: "Peso muerto", series: 3, repeticiones: 6, peso: 36 },
+            { nombre: "Floor press con barra", series: 4, repeticiones: 8, peso: 24 },
+            { nombre: "Remo con barra", series: 4, repeticiones: 10, peso: 24 },
+            { nombre: "Press militar de pie", series: 3, repeticiones: 8, peso: 20 },
+            { nombre: "Pájaros", series: 3, repeticiones: 17, peso: 4 },
+            { nombre: "Zancadas", series: 3, repeticiones: 15, peso: 6 },
+        ],
+    },
+    {
+        fecha: "22/09",
+        ejercicios: [
+            { nombre: "Sentadillas", series: 3, repeticiones: 8, peso: 26 },
+            { nombre: "Peso muerto rumano", series: 3, repeticiones: 10, peso: 32 },
+        ],
+    },
+];
