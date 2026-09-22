067675cd83d6f7562018c99372e1fc576d4eb543
Alberto Sánchez <albesandicode@gmail.com>
2026-09-22 17:07:37 +0200

    Genera el detalle del primer entrenamiento y renombra de variables

--- app.js
+++ app.js
@@ -1,45 +1,58 @@
 let entrenamientos = [
-    {
-        fecha: "19/09",
-        ejercicios: [
-            { nombre: "Peso muerto", series: 3, repeticiones: 6, peso: 36 },
-            { nombre: "Floor press con barra", series: 4, repeticiones: 8, peso: 24 },
-            { nombre: "Remo con barra", series: 4, repeticiones: 10, peso: 24 },
-            { nombre: "Press militar de pie", series: 3, repeticiones: 8, peso: 20 },
-            { nombre: "Pájaros", series: 3, repeticiones: 17, peso: 4 },
-            { nombre: "Zancadas", series: 3, repeticiones: 15, peso: 6 },
-        ],
-    },
-    {
-        fecha: "20/09",
-        ejercicios: [
-            { nombre: "Sentadillas", series: 3, repeticiones: 8, peso: 26 },
-            { nombre: "Peso muerto rumano", series: 3, repeticiones: 10, peso: 32 },
-        ],
-    },
-    {
-        fecha: "21/09",
-        ejercicios: [
-            { nombre: "Peso muerto", series: 3, repeticiones: 6, peso: 38 },
-            { nombre: "Floor press con barra", series: 4, repeticiones: 8, peso: 28 },
-            { nombre: "Remo con barra", series: 4, repeticiones: 10, peso: 26 },
-            { nombre: "Press militar de pie", series: 3, repeticiones: 8, peso: 22 },
-        ]
-    }
+  {
+    fecha: "19/09",
+    ejercicios: [
+      { nombre: "Peso muerto", series: 3, repeticiones: 6, peso: 36 },
+      { nombre: "Floor press con barra", series: 4, repeticiones: 8, peso: 24 },
+      { nombre: "Remo con barra", series: 4, repeticiones: 10, peso: 24 },
+      { nombre: "Press militar de pie", series: 3, repeticiones: 8, peso: 20 },
+      { nombre: "Pájaros", series: 3, repeticiones: 17, peso: 4 },
+      { nombre: "Zancadas", series: 3, repeticiones: 15, peso: 6 },
+    ],
+  },
+  {
+    fecha: "20/09",
+    ejercicios: [
+      { nombre: "Sentadillas", series: 3, repeticiones: 8, peso: 26 },
+      { nombre: "Peso muerto rumano", series: 3, repeticiones: 10, peso: 32 },
+    ],
+  },
+  {
+    fecha: "21/09",
+    ejercicios: [
+      { nombre: "Peso muerto", series: 3, repeticiones: 6, peso: 38 },
+      { nombre: "Floor press con barra", series: 4, repeticiones: 8, peso: 28 },
+      { nombre: "Remo con barra", series: 4, repeticiones: 10, peso: 26 },
+      { nombre: "Press militar de pie", series: 3, repeticiones: 8, peso: 22 },
+    ],
+  },
 ];
 
-/* for (const entrenamiento of entrenamientos) {
-    console.log(`${entrenamiento.fecha} - ${entrenamiento.ejercicios.length} ejercicios`);
-} */
+const historialEntrenamientos = document.getElementById("historial-entrenamientos");
 
-const lista = document.getElementById("lista-entrenamientos");
-
-let html = "";
+let htmlEntrenamientos = "";
 
 for (const entrenamiento of entrenamientos) {
-  html += `<li>${entrenamiento.fecha} - ${entrenamiento.ejercicios.length} ejercicios</li>`;
+  htmlEntrenamientos += `<li>
+        ${entrenamiento.fecha} - ${entrenamiento.ejercicios.length} ejercicios
+    </li>`;
 }
 
-lista.innerHTML = html;
+historialEntrenamientos.innerHTML = htmlEntrenamientos;
 
-// Intentos hasta que funciona: 2
+const filasEjercicios = document.getElementById("filas-ejercicios");
+
+let htmlEjercicios = "";
+
+for (const ejercicio of entrenamientos[0].ejercicios) {
+  htmlEjercicios += `<tr>
+        <td>${ejercicio.nombre}</td>
+        <td>${ejercicio.series}</td>
+        <td>${ejercicio.repeticiones}</td>
+        <td>${ejercicio.peso}</td>
+    </tr>`;
+}
+
+filasEjercicios.innerHTML = htmlEjercicios;
+
+// Intentos hasta que funciona: 5
